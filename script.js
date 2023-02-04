4f0dab7ca987118cf00f8890bea105a3da35b682
alola-uni <124448594+alola-uni@users.noreply.github.com>
2023-02-04 20:26:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+gsap.from("#herotitle h1", {
+    opacity: 0,
+    y:20,
+    duration:1.4
+})
+gsap.from("#bigherotitle", {
+    opacity: 0,
+    y:20,
+    duration:1.4,
+    delay:1
+})
+gsap.from("#herotitle h2", {
+    opacity: 0,
+    y:20,
+    duration:1.4,
+    delay:1
+})
+gsap.from("#recognition, #heroimage", {
+    opacity: 0,
+    y:20,
+    duration:1.4,
+    delay:2
+})
